99f784b593e55b77c359a1770cf89c6a2f1dc422
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2016-07-17 16:06:50 +0200

    Week 3, Lesson 5, Assignment 3: Peer Graded Assignment: Resolve and Local Storage Instructions
    In this assignment you will be concentrating on using ui-router's resolve object to update the app. In addition you will use the local storage to persist the information about the user's favorite dishes. The three tasks to be accomplished in this assignment are as follows:
    
    Task 1
    - Update the home, menu and aboutus states in app.js to use resolve to support data resolution in the ui-router.
    
    Task 2
    - Update IndexController, MenuController and AboutController to inject the data obtained through resolution and simplify the controller code
    
    Task 3
    - Make use of local storage support in the app to persist the user's favorite dishes on the device. The information should persist across app shut down and restart.
    Changes to be committed:
    	modified:   www/js/controllers.js
    	modified:   www/js/services.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,7 +11,7 @@ angular.module('conFusion.controllers', [])
 
     // Form data for the login modal
     $scope.loginData = $localStorage.getObject('userinfo', '{}');
-    
+
     // Create the login modal that we will use later
     $ionicModal.fromTemplateUrl('templates/login.html', {
         scope: $scope
