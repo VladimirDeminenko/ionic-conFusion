fd1b67170f9097e55300ee71473321b7b9c5da42
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2016-07-24 00:23:33 +0200

    Week 4, Lesson 1, Exercise 1: Customizing and Controlling the Splash Screen
    Objectives and Outcomes
    
    In this exercise you will learn how to customize the splash screen and also control the duration of its display on the screen. To do this, you will make use of Cordova through the ngCordova splash screen support. At the end of this exercise, you will be able to:
    
    - Customize the splash screen of an Ionic application
    - Control the display of the splash screen through the Cordova plugin support
    
      Conclusions
    
      In this exercise, you learnt the use of your first ngCordova and Cordova plugin in order to customize and control the splash screen.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@ angular.module('conFusion', ['ionic', 'ngCordova', 'conFusion.controllers', 'con
         $ionicPlatform.ready(function () {
             $timeout(function () {
                 $cordovaSplashscreen.hide();
-            }, 20000);
+            }, 2000);
         });
 
         $rootScope.$on('loading:show', function () {
