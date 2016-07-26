f948a6d58625ff170a5f43647a495519af3aeae0
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2016-07-26 22:10:06 +0200

    Week 4, Lesson 5, Peer Graded Assignment 4: Cordova and ngCordova
    Instructions
    
    The final assignment concentrates on Cordova plugins and ngCordova wrappers of these plugins. You can find the links to these plugins in teh additional resources. This assignment consists of three tasks:
    
    Task 1
    
    In this task you will be using the Cordova Image Picker plugin to support the selection of the user's photo for the registration page from the photos on the device.
    
    - The Image Picker plugin will enable you to select a picture from the picture gallery and add it to the registration page.
    - You need to add another button named Gallery, below the Take Picture button in the registration page.
    
    Task 2
    
    In this task you will add the Cordova notifications and Toast to show the notification and toast message
    
    - When the user selects an item from the dish details page and adds it to the favorites, put a notification in the notification bar
    - When the user selects an item from the dish details page and adds it to the favorites, show a toast message at the bottom of the screen.
    
    Task 3
    
    In this task you will make the phone vibrate when an item is deleted from our favorites, using the Cordova vibate plugin.
    
    - When the user deletes an item from the list of favorites, then you will vibrate the phone to indicate that the item will be deleted. The vibration should be caused only when the user approves the deletion of the item using the confirm popup.
    
    Review criterialess
    
    Your submission will be graded based on the following criteria:
    
    Task 1
    
    - The registration page should now include a new button named Gallery below the Take Picture button.
    - Clicking on the Gallery button should bring up the image gallery from which the user should be able to select a picture to include in the registration page
    - The appropriate ngCordova wrapper service is properly injected in the AppCtrl controller
    
    Task 2
    
    - When the user clicks on the Add To Favorites option in the Popover in the dish details page, a notification should be added to the notification bar with the information of the dish
    - When the user clicks on the Add To Favorites option in the Popover in the dish details page, a toast message is shown at the bottom of the screen showing the details of the dish added to the favorites
    - The appropriate ngCordova wrapper service is properly injected in the DishDetailController controller.
    
    Task 3
    
    - The appropriate ngCordova wrapper service is properly injected in the FavoritesController controller.
    - The phone is vibrated when the user confirms that the item should be deleted.
    
    Changes to be committed:
    modified: www/js/controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -132,7 +132,7 @@ angular.module('conFusion.controllers', [])
             height: 100,
             quality: 50
         };
-        
+
         $scope.selectPicture = function () {
             $cordovaImagePicker.getPictures(selectOptions).then(function (results) {
                 $scope.registration.imgSrc = results[0];
@@ -178,25 +178,26 @@ angular.module('conFusion.controllers', [])
         favoriteFactory.addToFavorites(index);
         $ionicListDelegate.closeOptionButtons();
 
-        //        $ionicPlatform.ready(function () {
-        $cordovaLocalNotification.schedule({
-            id: 1,
-            title: "Added Favorite",
-            text: $scope.dishes[index].name
-        }).then(function () {
-                console.log('Added Favorite ' + $scope.dishes[index].name);
-            },
-            function () {
-                console.log('Failed to add Notification ');
-            });
+        $ionicPlatform.ready(function () {
+            $cordovaLocalNotification.schedule({
+                id: 1,
+                title: "Added Favorite",
+                text: $scope.dishes[index].name
+            }).then(function () {
+                    console.log('Added Favorite ' + $scope.dishes[index].name);
+                },
+                function () {
+                    console.log('Failed to add Notification ');
+                });
 
-        $cordovaToast
-            .show('Added Favorite ' + $scope.dishes[index].name, 'long', 'center')
-            .then(function (success) {
-                // success
-            }, function (error) {
-                // error
-            });
+            $cordovaToast
+                .show('Added Favorite ' + $scope.dishes[index].name, 'long', 'center')
+                .then(function (success) {
+                    // success
+                }, function (error) {
+                    // error
+                });
+        });
     };
 }])
 
